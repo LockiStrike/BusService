d74aec42f07bc52fd18ec0bdc02f398cc13ed9dd
Vlad Bilanchuk <burning1848@gmail.com>
2017-05-20 18:54:54 +0300

    added all functionality for USER and tested

--- pivorak.rb
+++ pivorak.rb
@@ -2,8 +2,85 @@ require 'mysql'
 
 require_relative 'db_data'
 
-module BusList
-		def  show_bus_list(start_place=nil, start_date=nil)
+
+# module BusList
+# 		def  show_bus_list(start_place=nil, start_date=nil)
+# 		query = "SELECT * FROM BusShedule "
+# 		if start_place != nil and start_date != nil
+# 			query += "WHERE start_place = '%s' AND start_date = '%s'" % [start_place, start_date]
+# 		elsif start_place == nil and start_date != nil
+# 			query += "WHERE start_date = '%s'" % start_date
+# 		elsif start_place != nil and start_date == nil
+# 			query += "WHERE start_place = '%s'" % start_place
+# 		end
+		
+# 		bus_list = @connect.query(query)
+
+# 		while bus = bus_list.fetch_hash do
+# 			print_bus(bus)
+# 		end
+		
+		
+# 	end
+
+# 	def print_bus(bus)
+# 		printf "%-15s %-20s %-20s %-15s %-15s %-15s %-15s %-15s %s \n" % [bus['number'], bus['from_place'], bus['to_place'], \
+# 													 bus['start_date'], bus['start_time'], bus['end_date'], \
+# 													 bus['end_time'], bus['seats_count'], bus['avaliable_seats_count'] ]
+# 	end
+# end
+
+module Common_func
+	def free_login?(user_login)
+		query = "SELECT login FROM Accounts WHERE login = '%s'" % user_login
+		logins = @connect.query(query)
+		if logins.fetch_row != nil
+			puts 'Sorry this login is already exist'
+			false
+		else
+			true
+		end
+
+	end
+end
+
+
+class User
+
+	include Db_data
+	include Common_func
+	# include BusList
+	def initialize()
+		@connect = Mysql.new(host, user, password, database)
+	end
+	
+
+	def log_in(user_name, user_password)
+		query = "SELECT * FROM Accounts WHERE login='%s' AND password='%s'" % [user_name, user_password]
+		acc_exist = @connect.query(query)
+		if acc_exist.fetch_row != nil
+			@login = user_name
+			puts "Log in successful"
+			true
+		else
+			puts "login or password is incorrect, try once more"
+			false
+		end
+	end
+	
+	def registration(user_login, user_password)
+		if free_login?(user_login)
+			@connect.query("INSERT INTO Accounts(login, password, is_admin) VALUES('%s', '%s', false)" % [user_login, user_password])
+			@login = user_login
+			puts 'registration successful'
+			true
+		end
+		
+		false
+	end
+	
+
+	def  show_bus_list(start_place=nil, start_date=nil)
 		query = "SELECT * FROM BusShedule "
 		if start_place != nil and start_date != nil
 			query += "WHERE start_place = '%s' AND start_date = '%s'" % [start_place, start_date]
@@ -18,51 +95,103 @@ module BusList
 		while bus = bus_list.fetch_hash do
 			print_bus(bus)
 		end
-		
-		
+			
 	end
 
+	def buy_ticket(number, position)
+		# query = "SELECT avaliable_seats_count FROM BusShedule WHERE number = '%s'" % number
+		# seats_count = @connect.query(query).fetch_hash['avaliable_seats_count']
+		# if position.to_i < 1 || position.to_i > seats_count.to_i
+		# 	puts "Incorrect seats number, try once more"
+		# 	return
+		# end
+
+		query = "SELECT * FROM BusShedule WHERE number = '%s'" % number
+		bus_result = @connect.query(query)
+		bus = bus_result.fetch_hash
+		if bus != nil
+			if position.to_i > 0 && position.to_i <= bus['seats_count'].to_i
+				if (bus['avaliable_seats_count'].to_i > 0)
+					if free_position?(number, position)
+						query = "INSERT INTO BoughtTickets(number, position, login) VALUES('%s', '%s', '%s')" % [number, position, @login]
+						@connect.query(query)
+
+						query = "UPDATE BusShedule SET avaliable_seats_count = '%s' WHERE number = '%s'" % [bus['avaliable_seats_count'].to_i - 1, number]
+						@connect.query(query)
+						puts "Ticket is successfuly bought"
+						return true
+					else
+						return false
+					end
+				else
+					puts "Sorry, all tickets for this bus already bought"
+					return false
+				end
+			else
+				puts "Incorrect seat number, try once more"
+				return false
+			end
+		else
+			puts "Sorry, bus with this number doesn't exist, try once more"
+			return false
+		end
+	end
+
+	def show_bought_tickets
+		query = "SELECT * FROM BoughtTickets WHERE login = '%s'" % @login
+		tickets = @connect.query(query)
+		while ticket = tickets.fetch_hash do
+			bus_query = "SELECT * FROM BusShedule WHERE number = '%s'" % ticket['number']
+			bus = @connect.query(bus_query)
+			print_bus(bus.fetch_hash)
+			puts "Your seat: %s" % ticket['position']
+		end
+	end
+
+	def show_seats_map(bus_number)
+		query = "SELECT position FROM BoughtTickets WHERE number = '%s' " % bus_number
+		seats = @connect.query(query)
+		taken_seats = []
+		while seat = seats.fetch_row do
+			taken_seats = []
+		end
+	end
+
+
+
+private
 	def print_bus(bus)
 		printf "%-15s %-20s %-20s %-15s %-15s %-15s %-15s %-15s %s \n" % [bus['number'], bus['from_place'], bus['to_place'], \
 													 bus['start_date'], bus['start_time'], bus['end_date'], \
 													 bus['end_time'], bus['seats_count'], bus['avaliable_seats_count'] ]
 	end
-end
 
-def free_login?(user_login)
-	logins = @connect.query("SELECT login FROM UserDb where login = '%s'" % user_login)
-	if logins.fetch_row != nil
-		puts 'Sorry this login is already exist'
-		false
-	else
+	def free_position?(number, position)
+		query = "SELECT position FROM BoughtTickets WHERE number = '%s'" % number
+		bus_response = @connect.query(query)
+		bus = bus_response.fetch_hash
+		while bus != nil do
+			if position.to_i == bus['position'].to_i
+				puts "Sorry, this seat already taken"
+				return false 
+			end
+			bus = bus_response.fetch_hash
+		end
 		true
 	end
 
-end
-
-
-class User
-
-	include BusList
-	def initialize(user_login=nil)
-		@connect = Mysql.new(host, user, password, database)
-		@login = user_login
-	end
-	
-	
-	def registration(user_login, user_password)
-		@connect.query("INSERT INTO UserDb(login, password) VALUES('%s', '%s')" % [user_login, user_password])
-		puts 'registration successful'
-	end
-	
 
 end
+
+
 
 
 class Admin
-	include BusList
 
-	def initialize
+	include Db_data
+	# include BusList
+
+	def initialize()
 		@connect = Mysql.new(host, user, password, database)
 	end
 
--- testing.rb
+++ testing.rb
@@ -1,8 +1,45 @@
 require_relative 'pivorak'
-require_relative 'db_data'
+# require_relative 'db_data'
 
 
-db = Admin.new
+
+puts "Welcome to LockiStrike bus service!\n"
+puts "To make some action use /[command name]\n"
+puts "To see list of command use /commands"
+
+
+def start_working
+    puts "Do you want to log in( '/log in' ) in or registrate( '/registrate' )"
+
+end
+
+
+
+active_user = User.new
+active_user.log_in('Vlad', "1234")
+# active_user.show_bus_list
+# number = gets.chomp
+# position = gets.chomp
+# active_user.buy_ticket(number, position)
+# number = gets.chomp
+# position = gets.chomp
+# active_user.buy_ticket(number, position)
+active_user.show_bought_tickets
+
+# puts "registration\n"
+# begin
+# puts "name: "
+# name = gets.chomp
+# end while(not active_user.free_login?(name))
+# puts "password: "
+# password = gets.chomp
+# active_user.registration(name, password)
+# active_user.log_in(name, '2')
+# command = ""
+# begin
+    
+
+# end where 
 
 #begin
 #	puts "enter login to registrate: "
@@ -21,4 +58,10 @@ db = Admin.new
 # db.add_bus('Varshava', 'ksladfjaskldfjlsakdfjsadf', '25.08.2017', '15:40', '15.06.2017', '21:40', 200)
 # db.add_bus('Lviv', 'Kiev', '15.06.2017', '10:40', '17.06.2017', '16:40', 10)
 
-db.show_bus_list
+# db.show_bus_list
+
+
+
+# arr.each do |el|
+#     puts el
+# end
